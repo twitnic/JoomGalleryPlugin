4c204fbc7077d3181cf6bb5383f14942829c7965
Dominic Richter <kontakt@twitnic.de>
2026-02-21 02:16:52 +0100

    codestyle

--- services/provider.php
+++ services/provider.php
@@ -10,23 +10,22 @@ use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 use My\Plugin\Content\Joomgallery\Extension\JoomGalleryPlugin;
 
-return new class() implements ServiceProviderInterface
-{
-    public function register(Container $container)
-    {
-        $container->set(
-            PluginInterface::class,
-            function (Container $container) {
+return new class() implements ServiceProviderInterface {
+	public function register(Container $container)
+	{
+		$container->set(
+			PluginInterface::class,
+			function (Container $container) {
 
-                $config = (array) PluginHelper::getPlugin('content', 'joomgallery');
-                $subject = $container->get(DispatcherInterface::class);
-                $app = Factory::getApplication();
+				$config  = (array) PluginHelper::getPlugin('content', 'joomgallery');
+				$subject = $container->get(DispatcherInterface::class);
+				$app     = Factory::getApplication();
 
-                $plugin = new JoomGalleryPlugin($subject, $config);
-                $plugin->setApplication($app);
+				$plugin = new JoomGalleryPlugin($subject, $config);
+				$plugin->setApplication($app);
 
-                return $plugin;
-            }
-        );
-    }
+				return $plugin;
+			}
+		);
+	}
 };
--- src/Extension/JoomGalleryPlugin.php
+++ src/Extension/JoomGalleryPlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace My\Plugin\Content\Joomgallery\Extension;
 
 // no direct access
@@ -18,67 +19,70 @@ use Joomgallery\Component\Joomgallery\Administrator\Helper\JoomHelper;
 
 class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 {
-    protected $wa = false; 
+	protected $wa = false;
 
-    public static function getSubscribedEvents(): array
-    {
-        return [
-                'onContentPrepare' => 'replaceJGtags'  
-                ];
-    }
+	public static function getSubscribedEvents(): array
+	{
+		return [
+			'onContentPrepare' => 'replaceJGtags'
+		];
+	}
 
 	function renderLinks(&$text, $accept_legacy_tags)
 	{
 		$regex_link = '/href="joomgallery:([0-9]+)([a-z,A-Z,0-9,%,=,|, ]*)"/';
-		if ($accept_legacy_tags) {
+		if ($accept_legacy_tags)
+		{
 			$regex_link = '/href="(?:joomgallery|joomplulink):([0-9]+)([a-z,A-Z,0-9,%,=,|, ]*)"/';
 		}
-    	if(preg_match_all($regex_link, $text, $matches, PREG_SET_ORDER))
-    	{
-			foreach($matches as $match)
+		if (preg_match_all($regex_link, $text, $matches, PREG_SET_ORDER))
+		{
+			foreach ($matches as $match)
 			{
-				$type = 'image';
+				$type    = 'image';
 				$options = explode('|', trim($match[2]));
-            foreach ($options as $option) {
-               $opt = explode('=',$option);
-               if ($opt[0]=='type') {
+				foreach ($options as $option)
+				{
+					$opt = explode('=', $option);
+					if ($opt[0] == 'type')
+					{
 						$type = $opt[1];
 						if ($type == 'img') $type = 'image';
 						if ($type == 'thumb') $type = 'thumbnail';
 					}
-					if ($opt[0]=='view' && $opt[1]=='category') $type = 'category';
+					if ($opt[0] == 'view' && $opt[1] == 'category') $type = 'category';
 				}
-				$output = 'href="'.JoomHelper::getViewRoute($type,$match[1]).'"';
-				$text = str_replace($match[0], $output, $text);
+				$output = 'href="' . JoomHelper::getViewRoute($type, $match[1]) . '"';
+				$text   = str_replace($match[0], $output, $text);
 			}
 		}
 		$regex_catlink = '/href="joomgallerycat:([0-9]+)"/';
-    	if(preg_match_all($regex_catlink, $text, $matches, PREG_SET_ORDER))
-    	{
-			foreach($matches as $match)
+		if (preg_match_all($regex_catlink, $text, $matches, PREG_SET_ORDER))
+		{
+			foreach ($matches as $match)
 			{
-            $output = 'href="'.JoomHelper::getViewRoute('category',$match[1]).'"';
-        		$text = str_replace($match[0], $output, $text);
+				$output = 'href="' . JoomHelper::getViewRoute('category', $match[1]) . '"';
+				$text   = str_replace($match[0], $output, $text);
 			}
 		}
 	}
 
 	function renderTitles(&$text, $accept_legacy_tags)
 	{
-		$regex_alt  = '/alt="joomgallery:([0-9]+)"/';
+		$regex_alt = '/alt="joomgallery:([0-9]+)"/';
 
 		if (preg_match_all($regex_alt, $text, $matches, PREG_SET_ORDER))
 		{
-			foreach($matches as $match)
+			foreach ($matches as $match)
 			{
-				$image = JoomHelper::getRecord('image',$match[1]);
-				if(!is_null($image))
+				$image = JoomHelper::getRecord('image', $match[1]);
+				if (!is_null($image))
 				{
-					$output = 'alt="'.$image->title.'"';
+					$output = 'alt="' . $image->title . '"';
 				}
 				else
 				{
-					$output = 'alt="'.Text::_('PLG_JGAL_IMAGE_NOT_DISPLAYABLE').'"';
+					$output = 'alt="' . Text::_('PLG_JGAL_IMAGE_NOT_DISPLAYABLE') . '"';
 				}
 				$text = str_replace($match[0], $output, $text);
 			}
@@ -87,31 +91,35 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 
 	public function renderImages(&$text, $accept_legacy_tags)
 	{
-		$regex_tag  = '/{joomgallery:([0-9]+)([a-z,A-Z,0-9,%,=,|, ]*)}/';
+		$regex_tag = '/{joomgallery:([0-9]+)([a-z,A-Z,0-9,%,=,|, ]*)}/';
 
-		if ($accept_legacy_tags) {
+		if ($accept_legacy_tags)
+		{
 			$regex_tag = '/{(?:joomgallery|joomplu):([0-9]+)([a-z,A-Z,0-9,%,=,|, ]*)}/';
 		}
 
-		if(preg_match_all($regex_tag, $text, $matches, PREG_SET_ORDER))
+		if (preg_match_all($regex_tag, $text, $matches, PREG_SET_ORDER))
 		{
-    		$params = ComponentHelper::getParams('com_joomgallery');
-    		$caption_align = $params->get('jg_category_view_caption_align', 'center', 'STRING');
-			$align = '';
+			$params        = ComponentHelper::getParams('com_joomgallery');
+			$caption_align = $params->get('jg_category_view_caption_align', 'center', 'STRING');
+			$align         = '';
 
-			foreach($matches as $match)
+			foreach ($matches as $match)
 			{
-				$type = 'detail';
+				$type    = 'detail';
 				$catlink = false;
-				$linked = true;
-        
-				try {
-					$image = JoomHelper::getRecord('image',$match[1]);
-				} catch (Exeption $e) {
-					Factory::getApplication()->enqueueMessage($e->getMessage(),'error');
+				$linked  = true;
+
+				try
+				{
+					$image = JoomHelper::getRecord('image', $match[1]);
+				}
+				catch (Exeption $e)
+				{
+					Factory::getApplication()->enqueueMessage($e->getMessage(), 'error');
 				}
 
-				if($image)
+				if ($image)
 				{
 					$figclass = 'joom-image text-center';
 					$floatfig = true;
@@ -120,42 +128,46 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 						$linked = false;
 					}
 					$options = explode('|', trim($match[2]));
-					foreach ($options as $option) {
-						$opt = explode('=',$option);
-						if ($opt[0]=='type') {
+					foreach ($options as $option)
+					{
+						$opt = explode('=', $option);
+						if ($opt[0] == 'type')
+						{
 							$type = $opt[1];
 							if ($type == 'img') $type = 'image';
 							if ($type == 'orig') $type = 'original';
 							if ($type == 'thumb') $type = 'thumbnail';
 						}
-						if ($opt[0]=='linked' && $opt[1]=='0') $linked=false;
-						if ($opt[0]=='catlink' && $opt[1]) $catlink=true;
-						if ($opt[0]=='float' && $opt[1]=='0') $floatfig=false;
-						if ($opt[0]=='align' && $opt[1]) $align=$opt[1];
+						if ($opt[0] == 'linked' && $opt[1] == '0') $linked = false;
+						if ($opt[0] == 'catlink' && $opt[1]) $catlink = true;
+						if ($opt[0] == 'float' && $opt[1] == '0') $floatfig = false;
+						if ($opt[0] == 'align' && $opt[1]) $align = $opt[1];
 					}
-					if ($floatfig) {
-						if ($align=='left') $figclass = 'jg-image float-start';
-						if ($align=='right') $figclass = 'jg-image float-end';
+					if ($floatfig)
+					{
+						if ($align == 'left') $figclass = 'jg-image float-start';
+						if ($align == 'right') $figclass = 'jg-image float-end';
 					}
-					else {
-						if ($opt[1]=='left') $figclass='joom-image text-start';
-						if ($opt[1]=='right') $figclass='joom-image text-end';
+					else
+					{
+						if ($opt[1] == 'left') $figclass = 'joom-image text-start';
+						if ($opt[1] == 'right') $figclass = 'joom-image text-end';
 					}
-            
-					$imageurl = JoomHelper::getImg($match[1],$type);
+
+					$imageurl = JoomHelper::getImg($match[1], $type);
 					// TODO: add catlink if requested
 					// NB: joom-image has width:100% which prevents floating; check other classes?
 					$output = "<figure class=\"figure $figclass\">\n";
 					// Try this instead:  Route::_('index.php?option=com_joomgallery&view=image&id='.(int) $match[1])
-					if ($linked) $output .= '<a href="'.JoomHelper::getImg($match[1],'detail').'">';
-					$output .= '<img src="'.$imageurl.'" class="figure-img img-fluid rounded" alt="'.$image->title.'">'."\n";
+					if ($linked) $output .= '<a href="' . JoomHelper::getImg($match[1], 'detail') . '">';
+					$output .= '<img src="' . $imageurl . '" class="figure-img img-fluid rounded" alt="' . $image->title . '">' . "\n";
 					if ($linked) $output .= '</a>';
-					if (strpos($match[2], 'caption')) $output .= '<figcaption class="figure-caption '.$caption_align.'">'."{$image->title}</figcaption>\n";
+					if (strpos($match[2], 'caption')) $output .= '<figcaption class="figure-caption ' . $caption_align . '">' . "{$image->title}</figcaption>\n";
 					$output .= "</figure>\n";
 				}
 				else
 				{
-					$output = '<p><b>'.Text::_('PLG_JGAL_IMAGE_NOT_DISPLAYABLE').'</b></p>';
+					$output = '<p><b>' . Text::_('PLG_JGAL_IMAGE_NOT_DISPLAYABLE') . '</b></p>';
 				}
 
 				$text = str_replace($match[0], $output, $text);
@@ -165,73 +177,75 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 
 	public function renderCat(&$text, $accept_legacy_tags)
 	{
-		$regex_cat  = '/{joomgallerycat:([0-9]+)([a-z,0-9,=,",|, ]*)}/';
+		$regex_cat = '/{joomgallerycat:([0-9]+)([a-z,0-9,=,",|, ]*)}/';
 
-		if ($accept_legacy_tags) {
-			$regex_cat  = '/{(?:joomgallerycat|joomplucat):([0-9]+)([a-z,0-9,=,",|, ]*)}/';
+		if ($accept_legacy_tags)
+		{
+			$regex_cat = '/{(?:joomgallerycat|joomplucat):([0-9]+)([a-z,0-9,=,",|, ]*)}/';
 		}
 
-		if(preg_match_all($regex_cat, $text, $matches, PREG_SET_ORDER))
+		if (preg_match_all($regex_cat, $text, $matches, PREG_SET_ORDER))
 		{
-			foreach($matches as $match)
+			foreach ($matches as $match)
 			{
-				$app = \Joomla\CMS\Factory::getApplication();
+				$app         = \Joomla\CMS\Factory::getApplication();
 				$joomgallery = $app->bootComponent('com_joomgallery')->getMVCFactory();
 
-				$catModel = $joomgallery->createModel('Category','site');
+				$catModel = $joomgallery->createModel('Category', 'site');
 
-				$catid = (int)$match[1];
+				$catid = (int) $match[1];
 				$catModel->getItem($catid);
-				$catModel->component->createConfig('com_joomgallery.category' , $catid, true);
+				$catModel->component->createConfig('com_joomgallery.category', $catid, true);
 				$configs = $catModel->component->getConfig();
 				// Subcategory params
-				$subcategory_class          = $configs->get('jg_category_view_subcategory_class', 'masonry', 'STRING');
+				$subcategory_class = $configs->get('jg_category_view_subcategory_class', 'masonry', 'STRING');
 
 				// Image params
-				$category_class   = $configs->get('jg_category_view_class', 'masonry', 'STRING');;
-				$num_columns      = $configs->get('jg_category_view_num_columns', 6, 'INT');
-				$image_type       = $configs->get('jg_category_view_type_images', 'thumbnail', 'STRING');
-				$caption_align    = $configs->get('jg_category_view_caption_align', 'right', 'STRING');
-				$image_class      = $configs->get('jg_category_view_image_class', '', 'STRING');
-				$justified_height = $configs->get('jg_category_view_justified_height', 320, 'INT');
-				$justified_gap    = $configs->get('jg_category_view_justified_gap', 5, 'INT');
-				$show_title       = $configs->get('jg_category_view_images_show_title', 0, 'INT');
-				$use_pagination   = $configs->get('jg_category_view_pagination', 0, 'INT');
-				$reloaded_images  = $configs->get('jg_category_view_number_of_reloaded_images', 3, 'INT');
-				$image_link       = $configs->get('jg_category_view_image_link', 'defaultview', 'STRING');
-				$title_link       = $configs->get('jg_category_view_title_link', 'defaultview', 'STRING');
-				$lightbox_image   = $configs->get('jg_category_view_lightbox_image', 'detail', 'STRING');
+				$category_class = $configs->get('jg_category_view_class', 'masonry', 'STRING');;
+				$num_columns         = $configs->get('jg_category_view_num_columns', 6, 'INT');
+				$image_type          = $configs->get('jg_category_view_type_images', 'thumbnail', 'STRING');
+				$caption_align       = $configs->get('jg_category_view_caption_align', 'right', 'STRING');
+				$image_class         = $configs->get('jg_category_view_image_class', '', 'STRING');
+				$justified_height    = $configs->get('jg_category_view_justified_height', 320, 'INT');
+				$justified_gap       = $configs->get('jg_category_view_justified_gap', 5, 'INT');
+				$show_title          = $configs->get('jg_category_view_images_show_title', 0, 'INT');
+				$use_pagination      = $configs->get('jg_category_view_pagination', 0, 'INT');
+				$reloaded_images     = $configs->get('jg_category_view_number_of_reloaded_images', 3, 'INT');
+				$image_link          = $configs->get('jg_category_view_image_link', 'defaultview', 'STRING');
+				$title_link          = $configs->get('jg_category_view_title_link', 'defaultview', 'STRING');
+				$lightbox_image      = $configs->get('jg_category_view_lightbox_image', 'detail', 'STRING');
 				$lightbox_thumbnails = $configs->get('jg_lightbox_thumbnails', 0, 'INT');
 				$lightbox_zoom       = $configs->get('jg_lightbox_zoom', 0, 'INT');
-				$show_description = $configs->get('jg_category_view_show_description', 0, 'INT');
-				$show_imgdate     = $configs->get('jg_category_view_show_imgdate', 0, 'INT');
-				$show_imgauthor   = $configs->get('jg_category_view_show_imgauthor', 0, 'INT');
-				$show_tags        = $configs->get('jg_category_view_show_tags', 0, 'INT');
+				$show_description    = $configs->get('jg_category_view_show_description', 0, 'INT');
+				$show_imgdate        = $configs->get('jg_category_view_show_imgdate', 0, 'INT');
+				$show_imgauthor      = $configs->get('jg_category_view_show_imgauthor', 0, 'INT');
+				$show_tags           = $configs->get('jg_category_view_show_tags', 0, 'INT');
 
-				$options = explode('|', trim($match[2]));
+				$options     = explode('|', trim($match[2]));
 				$max_entries = 0;
-				foreach ($options as $option) {
-					$opt = explode('=',$option);
-					if ($opt[0]=='columns') $num_columns=$opt[1];
-					if ($opt[0]=='limit') $max_entries=$opt[1];
+				foreach ($options as $option)
+				{
+					$opt = explode('=', $option);
+					if ($opt[0] == 'columns') $num_columns = $opt[1];
+					if ($opt[0] == 'limit') $max_entries = $opt[1];
 				}
 
 				if (!is_null($catitem = $catModel->item))
-				{ 
+				{
 					// Import CSS & JS
-					if($subcategory_class == 'masonry' || $category_class == 'masonry')
+					if ($subcategory_class == 'masonry' || $category_class == 'masonry')
 					{
 						$this->wa->useScript('com_joomgallery.masonry');
 					}
 
-					if($category_class == 'justified')
+					if ($category_class == 'justified')
 					{
 						$this->wa->useScript('com_joomgallery.justified');
 						$this->wa->addInlineStyle('.jg-images[class*=" justified-"] .jg-image-caption-hover { right: ' . $justified_gap . 'px; }');
 					}
 
 					$lightbox = false;
-					if($image_link == 'lightgallery' || $title_link == 'lightgallery')
+					if ($image_link == 'lightgallery' || $title_link == 'lightgallery')
 					{
 						$lightbox = true;
 
@@ -251,7 +265,7 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 					$numb_images = count($catimages);
 
 					// Add and initialize the grid script
-					$iniJS  = 'window.joomGrid["1-'.$catitem->id.'"] = {';
+					$iniJS = 'window.joomGrid["1-' . $catitem->id . '"] = {';
 					$iniJS .= '  itemid: "1-' . $catitem->id . '",';
 					$iniJS .= '  pagination: ' . $use_pagination . ',';
 					$iniJS .= '  layout: "' . $category_class . '",';
@@ -259,31 +273,31 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 					$iniJS .= '  numb_images: ' . $numb_images . ',';
 					$iniJS .= '  reloaded_images: ' . $reloaded_images . ',';
 					$iniJS .= '  lightbox: ' . ($lightbox ? 'true' : 'false') . ',';
-					$iniJS .= '  lightbox_params: {container: "lightgallery-1-'.$catitem->id.'", selector: ".lightgallery-item"},';
+					$iniJS .= '  lightbox_params: {container: "lightgallery-1-' . $catitem->id . '", selector: ".lightgallery-item"},';
 					$iniJS .= '  thumbnails: ' . ($lightbox_thumbnails ? 'true' : 'false') . ',';
 					$iniJS .= '  zoom: ' . ($lightbox_zoom ? 'true' : 'false') . ',';
-					$iniJS .= '  justified: {height: '.$justified_height.', gap: '.$justified_gap.'}';
+					$iniJS .= '  justified: {height: ' . $justified_height . ', gap: ' . $justified_gap . '}';
 					$iniJS .= '};';
 
 					$this->wa->useScript('com_joomgallery.joomgrid');
 					$this->wa->addInlineScript($iniJS, ['position' => 'after'], [], ['com_joomgallery.joomgrid']);
 
 					$children = $catModel->getChildren($catid);
-					$imgsData = [ 'id' => '1-'.(int) $catitem->id, 'layout' => $category_class, 'items' => $catimages, 'num_columns' => (int) $num_columns,
-                  'caption_align' => $caption_align, 'image_class' => $image_class, 'image_type' => $image_type, 'lightbox_type' => $lightbox_image, 'image_link' => $image_link,
-                  'image_title' => (bool) $show_title, 'title_link' => $title_link, 'image_desc' => (bool) $show_description, 'image_date' => (bool) $show_imgdate,
-                  'image_author' => (bool) $show_imgauthor, 'image_tags' => (bool) $show_tags
-                ];
-					$output = LayoutHelper::render('joomgallery.grids.images', $imgsData, null, array('component' => 'com_joomgallery'));
-					$output .= "<script>\n".
-						"  // Add event listener to images\n".
-						"  let loadImg = function() {\n".
-						"    this.closest('.jg-image').classList.add('loaded');\n".
-						"  }\n".
-						"  let images = Array.from(document.getElementsByClassName('jg-image-thumb'));\n".
-						"  images.forEach(image => {\n".
-						"    image.addEventListener('load', loadImg);\n".
-						"  });\n".
+					$imgsData = ['id'            => '1-' . (int) $catitem->id, 'layout' => $category_class, 'items' => $catimages, 'num_columns' => (int) $num_columns,
+					             'caption_align' => $caption_align, 'image_class' => $image_class, 'image_type' => $image_type, 'lightbox_type' => $lightbox_image, 'image_link' => $image_link,
+					             'image_title'   => (bool) $show_title, 'title_link' => $title_link, 'image_desc' => (bool) $show_description, 'image_date' => (bool) $show_imgdate,
+					             'image_author'  => (bool) $show_imgauthor, 'image_tags' => (bool) $show_tags
+					];
+					$output   = LayoutHelper::render('joomgallery.grids.images', $imgsData, null, array('component' => 'com_joomgallery'));
+					$output   .= "<script>\n" .
+						"  // Add event listener to images\n" .
+						"  let loadImg = function() {\n" .
+						"    this.closest('.jg-image').classList.add('loaded');\n" .
+						"  }\n" .
+						"  let images = Array.from(document.getElementsByClassName('jg-image-thumb'));\n" .
+						"  images.forEach(image => {\n" .
+						"    image.addEventListener('load', loadImg);\n" .
+						"  });\n" .
 						"</script>\n";
 				}
 				else
@@ -297,13 +311,14 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 
 	public function replaceJGtags(Event $event)
 	{
-		if (!$this->getApplication()->isClient('site')) {
+		if (!$this->getApplication()->isClient('site'))
+		{
 			return;
 		}
-         
+
 		[$context, $article, $params, $page] = array_values($event->getArguments());
 		//if ($context !== "com_content.article" && $context !== "com_content.featured") return;
-        
+
 		$text = $article->text; // text of the article
 
 		if (strpos($article->text, 'joomgallery') === false && strpos($article->text, 'joomplu') === false)
@@ -317,15 +332,15 @@ class JoomGalleryPlugin extends CMSPlugin implements SubscriberInterface
 		$language->load('JoomGalleryPlugin', JPATH_BASE . '/plugins/content/joomgallery');
 
 		// Check existence of JoomGallery and include the interface class
-		if(!\Joomla\CMS\Component\ComponentHelper::isEnabled('com_joomgallery'))
+		if (!\Joomla\CMS\Component\ComponentHelper::isEnabled('com_joomgallery'))
 		{
-			$output = '<p><b>'.JText::_('PLG_JGAL_JG_NOT_INSTALLED').'</b></p>';
-			$article->text  = $output.$article->text;
+			$output        = '<p><b>' . JText::_('PLG_JGAL_JG_NOT_INSTALLED') . '</b></p>';
+			$article->text = $output . $article->text;
 
 			return;
 		}
-   
-		$app = Factory::getApplication();
+
+		$app      = Factory::getApplication();
 		$this->wa = $app->getDocument()->getWebAssetManager();
 		$this->wa->getRegistry()->addExtensionRegistryFile('com_joomgallery');
 		$this->wa->useStyle('com_joomgallery.site');
